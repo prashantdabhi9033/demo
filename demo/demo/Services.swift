3f6705ad3e126331df857de92b2436d486aba273
Prashant Dabhi <iosmoweb@moweb.com>
2020-12-11 11:14:48 +0530

    This is a new commit for what I originally planned to be amended

---
+++ demo/demo/Services.swift
@@ -0,0 +1,19 @@
+//
+//  Services.swift
+//  demo
+//
+//  Created by Prashant Dabhi on 11/12/20.
+//
+
+import Foundation
+
+public class Service{
+    private init(){
+        
+    }
+    
+    public static func GetString(str : String)->String{
+        return str
+    }
+}
+
---
+++ demo/demo/demo.h
@@ -0,0 +1,18 @@
+//
+//  demo.h
+//  demo
+//
+//  Created by Prashant Dabhi on 11/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for demo.
+FOUNDATION_EXPORT double demoVersionNumber;
+
+//! Project version string for demo.
+FOUNDATION_EXPORT const unsigned char demoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <demo/PublicHeader.h>
+
+
